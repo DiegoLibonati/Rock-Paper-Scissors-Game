2ee8079983bfeca3136c20d38ac29d9b7c8f1232
Die <dlibonati@leafnoise.io>
2025-01-16 14:38:14 -0300

    feat: better css config and tests config

--- src/helpers/getElements.test.ts
+++ src/helpers/getElements.test.ts
@@ -1,25 +1,29 @@
 import { getElements } from "./getElements";
 
-import { OFFICIAL_BODY } from "../tests/jest.setup";
+import { OFFICIAL_BODY } from "../tests/jest.constants";
 
-beforeEach(() => {
-  document.body.innerHTML = OFFICIAL_BODY;
-});
+describe("getElements.ts", () => {
+  describe("General Tests.", () => {
+    beforeEach(() => {
+      document.body.innerHTML = OFFICIAL_BODY;
+    });
 
-afterEach(() => {
-  document.body.innerHTML = "";
-});
+    afterEach(() => {
+      document.body.innerHTML = "";
+    });
 
-test("It must render the elements of the document that the 'getElements' function exports.", () => {
-  const { imgsUserOptions, scoreIA, scorePlayer, textPlay, textResult } =
-    getElements();
+    test("It must render the elements of the document that the 'getElements' function exports.", () => {
+      const { imgsUserOptions, scoreIA, scorePlayer, textPlay, textResult } =
+        getElements();
 
-  expect(scoreIA).toBeInTheDocument();
-  expect(scorePlayer).toBeInTheDocument();
-  expect(textPlay).toBeInTheDocument();
-  expect(textResult).toBeInTheDocument();
+      expect(scoreIA).toBeInTheDocument();
+      expect(scorePlayer).toBeInTheDocument();
+      expect(textPlay).toBeInTheDocument();
+      expect(textResult).toBeInTheDocument();
 
-  for (let img of imgsUserOptions) {
-    expect(img).toBeInTheDocument();
-  }
+      for (let img of imgsUserOptions) {
+        expect(img).toBeInTheDocument();
+      }
+    });
+  });
 });
--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,7 +1,7 @@
 export const getElements = () => ({
-  imgsUserOptions: document.querySelectorAll(".option-to-play") as NodeList,
-  scorePlayer: document.querySelector(".score_player") as HTMLHeadingElement,
-  scoreIA: document.querySelector(".score_ia") as HTMLHeadingElement,
+  imgsUserOptions: document.querySelectorAll(".option__play") as NodeList,
+  scorePlayer: document.querySelector(".score__player") as HTMLHeadingElement,
+  scoreIA: document.querySelector(".score__ia") as HTMLHeadingElement,
   textResult: document.getElementById("text-result") as HTMLHeadingElement,
   textPlay: document.getElementById("text-play") as HTMLHeadingElement,
 });
