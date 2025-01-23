db8fc733f2ff8b72d50ac817df8ae5c413ff4b05
Die <dlibonati@leafnoise.io>
2025-01-23 10:37:08 -0300

    feat: BEM CSS

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,7 +1,7 @@
 export const getElements = () => ({
-  imgsUserOptions: document.querySelectorAll(".option__play") as NodeList,
-  scorePlayer: document.querySelector(".score__player") as HTMLHeadingElement,
-  scoreIA: document.querySelector(".score__ia") as HTMLHeadingElement,
+  imgsUserOptions: document.querySelectorAll(".game__content-choice") as NodeList,
+  scorePlayer: document.querySelector(".game__header-user-score") as HTMLHeadingElement,
+  scoreIA: document.querySelector(".game__header-ia-score") as HTMLHeadingElement,
   textResult: document.getElementById("text-result") as HTMLHeadingElement,
   textPlay: document.getElementById("text-play") as HTMLHeadingElement,
 });
