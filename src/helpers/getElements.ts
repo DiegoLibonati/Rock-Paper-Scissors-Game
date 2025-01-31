c7bc81ba5b25af7151ef78ee1b91f93c061096e1
Die <dlibonati@leafnoise.io>
2025-01-31 10:58:48 -0300

    fix: better bem css

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,7 +1,7 @@
 export const getElements = () => ({
-  imgsUserOptions: document.querySelectorAll(".game__content-choice") as NodeList,
-  scorePlayer: document.querySelector(".game__header-user-score") as HTMLHeadingElement,
-  scoreIA: document.querySelector(".game__header-ia-score") as HTMLHeadingElement,
+  imgsUserOptions: document.querySelectorAll(".game__choice") as NodeList,
+  scorePlayer: document.querySelector(".game__player-score") as HTMLHeadingElement,
+  scoreIA: document.querySelector(".game__ia-score") as HTMLHeadingElement,
   textResult: document.getElementById("text-result") as HTMLHeadingElement,
   textPlay: document.getElementById("text-play") as HTMLHeadingElement,
 });
